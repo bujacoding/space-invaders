81fdc5fe162d368a9ba24e2033dde8ca625b3750
daddy <developer.kss@gmail.com>
2021-09-21 10:43:39 +0900

    set windows position

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 import sys
 from pygame.locals import *
+import os
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = f'{1000},{200}'
 
 pygame.init()
 
-displaysurf = pygame.display.set_mode((480, 640))
+canvas = pygame.display.set_mode((480, 640))
 pygame.display.set_caption('space invaders')
 clock = pygame.time.Clock()
 
 ship = pygame.image.load('spaceship.png')
 a = False
 d = False
-x = displaysurf.get_width() / 2 - ship.get_width() / 2
-y = displaysurf.get_height() - ship.get_height()
+x = canvas.get_width() / 2 - ship.get_width() / 2
+y = canvas.get_height() - ship.get_height()
 
 # 사용자 입력
 # 연산
@@ -42,10 +45,12 @@ while True:
         x -= 5
     if d == True:
         x += 5
+    if x >= canvas.get_width() - ship.get_width():
+        x = canvas.get_width() - ship.get_width()
 
     # 그리기
-    displaysurf.fill((255,255,255))
-    displaysurf.blit(ship,(x, y))
+    canvas.fill((255,255,255))
+    canvas.blit(ship,(x, y))
 
     pygame.display.update()
 
