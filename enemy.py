4aa58cdcdc0bfdd2457f8c1de38e06e5cc2da908
buja-coding <developer.kss@gmail.com>
2021-09-24 07:36:48 +0900

    모두 다 내려가기

--- enemy.py
+++ enemy.py
@@ -11,6 +11,7 @@ class Enemy:
         self.image = pygame.image.load('res/sprite/enemy.png')
         self.x = 0
         self.y = 0
+        self.last_direction = left_to_right
 
     def set_x(self, x):
         self.x = x
@@ -21,19 +22,35 @@ class Enemy:
     def get_height(self):
         return self.image.get_height()
 
-    def update(self, canvas):
+    def is_reached(self, canvas) -> bool:
         global left_to_right
         if left_to_right:
-            self.x += 3
-            if canvas.get_width() < self.x + self.get_width():
-                self.y += self.get_height()
-                left_to_right = False
+            return canvas.get_width() < self.x + self.get_width()
+        else:
+            return self.x < 0
 
+    def need_to_go_down(self):
+        return self.last_direction != left_to_right
+
+    def update(self, canvas):
+
+        global left_to_right
+
+        # 정해진 방향으로 이동
+        if left_to_right:
+            self.x += 3
         else:
             self.x -= 3
-            if self.x < 0:
-                self.y += self.get_height()
-                left_to_right = True
+
+        # 끝에 닿았는가? -> 함수로 만들어 보자
+        if self.is_reached(canvas):
+            # 모두에게 알리기
+            left_to_right = not left_to_right
+
+        # 내려가야 하는가?
+        if self.need_to_go_down():
+            self.y += self.get_height()
+            self.last_direction = left_to_right
 
     def render(self, canvas):
         canvas.blit(self.image, (self.x, self.y))
