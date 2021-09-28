590f89edb535243d05e2bd14aaccd22a20fd4a74
buja-coding <developer.kss@gmail.com>
2021-09-28 16:55:14 +0900

    파이썬코딩도장 문제 풀이

---
+++ dojang/5.5.py
@@ -0,0 +1,6 @@
+# 5.5
+distance = 12
+result = 0.2467 * distance + 4.159
+
+print(int(result))
+
---
+++ dojang/5.6.py
@@ -0,0 +1,9 @@
+print(102 * 0.6 + 225)
+fd = False
+
+
+x: int = 23
+
+print(type(x))
+_fdd = "d"
+print(_fdd)
